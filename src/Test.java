4255317f0d2d21ef576a20cc5eb99996ac9c8632
ashutosh <ashutosh@promptcloud.com>
2017-12-28 21:54:56 +0530

    added new programs

--- src/Test.java
+++ src/Test.java
@@ -4,10 +4,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Pattern p = Pattern.compile(".sp");
-		Matcher m = p.matcher("as");
-		boolean b = m.matches();
-		System.out.println(b);
+		//Pattern p = Pattern.compile(".sp");
+		//Matcher m = p.matcher("as");
+		//boolean b = m.matches();
+		//System.out.println(b);
+		String s = "ashu";
+		String s1 = s.replace('a', 'b');
+		System.out.println(s);
 	}
 
 }
